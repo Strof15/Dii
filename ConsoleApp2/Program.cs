c94823566f72c4c578ed7d6b291df09cd959454e
Strof15 <70961668+Strof15@users.noreply.github.com>
2023-06-01 10:16:14 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            List<nobeldiajakClass> lista = new List<nobeldiajakClass>();
+
+            try
+            {
+                File.ReadAllLines("nobel.csv").Skip(1).ToList().ForEach(x => lista.Add(new nobeldiajakClass(x)));
+                Console.WriteLine(" jo a beolvasas");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("nem jo a beolvasas");
+               
+            }
+
+            //4. feladat
+            Console.WriteLine(lista.Select(n => n).Where(n => n.Tipus.Contains("irodalmi")));
+
+            //6. feladat
+
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/nobeldiajakClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class nobeldiajakClass
+    {
+        //evszam tipus keresztnev vezeteknev
+        int evszam;
+        string tipus;
+        string keresznev;
+        string vezeteknev;
+
+        public int Evszam { get => evszam; set => evszam = value; }
+        public string Tipus { get => tipus; set => tipus = value; }
+        public string Keresznev { get => keresznev; set => keresznev = value; }
+        public string Vezeteknev { get => vezeteknev; set => vezeteknev = value; }
+
+        public nobeldiajakClass(int evszam, string tipus, string keresznev, string vezeteknev)
+        {
+            this.evszam = evszam;
+            this.tipus = tipus;
+            this.keresznev = keresznev;
+            this.vezeteknev = vezeteknev;
+        }
+
+        public nobeldiajakClass(string CsvSorok)
+        {
+            var mezo = CsvSorok.Split(';');
+            this.evszam = int.Parse(mezo[0]);
+            this.tipus = mezo[1];
+            this.keresznev = mezo[2];
+            this.vezeteknev = mezo[3];
+
+
+        }
+
+        
+    }
+}
